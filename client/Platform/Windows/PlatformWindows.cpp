a43b94a38a5e4140990ebd6af329c358a7e0449b
mos9527 <komahuang@tencent.com>
2026-07-27 17:39:40 +0800

    Overrride default `operator new` on Windows Client builds

--- client/Platform/Windows/PlatformWindows.cpp
+++ client/Platform/Windows/PlatformWindows.cpp
@@ -3,6 +3,18 @@
 #include <mdr/Protocol.hpp>
 #include "../Platform.hpp"
 
+
+// Override global operator new for reference Windows Client only
+// As to why this is needed, MS thought it's a good idea to make the default non-throwing operator new
+// a try-catch, around the throwing operator new.
+// Find it here and see for yourself
+//   C:\Program Files\Microsoft Visual
+//   Studio\18\Community\VC\Tools\MSVC\14.51.36231\crt\src\vcruntime\new_scalar_nothrow.cpp
+#include <mdr/Protocol.hpp>
+void* operator new(std::size_t n) noexcept { return mdr::MDRAllocator<char>().allocate(n); }
+void* operator new(size_t const n, std::nothrow_t const&) noexcept { return mdr::MDRAllocator<char>().allocate(n); }
+void operator delete(void* p) noexcept { mdr::MDRAllocator<char>().deallocate(static_cast<char*>(p)); }
+
 static MDRConnectionWindows* gConnClassic = nullptr;
 static MDRConnectionWindowsBLE* gConnBLE = nullptr;
 extern "C" {
